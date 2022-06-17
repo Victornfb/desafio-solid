d109f2bbef3340ec46dbff6d548c76b6734cde34
Victornfb <victornfb@outlook.com>
2022-06-17 15:50:59 -0300

    Refactor: id variable inside try block

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,13 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-
     try {
-      const users = this.listAllUsersUseCase.execute({ user_id } as {
-        user_id: string;
+      const { user_id } = request.headers;
+      const users = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString(),
       });
-      return response.json(users);
+      return response.status(200).json(users);
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
