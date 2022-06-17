2c461fae5ff32137345c4491931a61b5eecdb08a
Victornfb <victornfb@outlook.com>
2022-06-17 01:27:31 -0300

    Challenge completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User ${user_id} not found`);
+    }
+
+    if (user.admin === false) {
+      throw new Error(`User ${user_id} is not an administrator`);
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,16 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const { name, email, admin } = this.turnUserAdminUseCase.execute({
+        user_id,
+      });
+      return response.status(200).json({ name, email, admin });
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
